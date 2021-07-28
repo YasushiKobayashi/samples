12158e336efd76d5d8df53a3c2450da442612415
YasushiKobayashi <ptpadan@gmail.com>
2021-07-28 20:47:28 +0900

    update next config

---
+++ src/unit-test-refactor/next.config.js
@@ -0,0 +1,13 @@
+const next = {
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,
+  url: false,
+  typescript: {
+    ignoreDevErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = next
