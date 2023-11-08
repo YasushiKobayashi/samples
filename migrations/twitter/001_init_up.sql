0a8bdb0e0704eb3564ae270b5c59fbd5aba089aa
YasushiKobayashi <ptpadan@gmail.com>
2023-11-08 19:10:08 +0900

    update

--- migrations/twitter/001_init_up.sql
+++ migrations/twitter/001_init_up.sql
@@ -14,7 +14,6 @@ CREATE TABLE `users` (
 CREATE TABLE `user_profiles` (
     `user_id` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE current_timestamp,
     `deleted` BOOLEAN NULL DEFAULT false,
@@ -22,5 +21,18 @@ CREATE TABLE `user_profiles` (
 
     CONSTRAINT `user_profile_user_id_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
     UNIQUE INDEX `user_profiles_user_id_key`(`user_id`),
+    INDEX `user_id`(`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `user_personal_informations` (
+    `user_id` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE current_timestamp,
+    `deleted` BOOLEAN NULL DEFAULT false,
+    `deleted_date` TIMESTAMP NULL,
+
+    CONSTRAINT `user_personal_informations_user_id_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+    UNIQUE INDEX `user_personal_informations_user_id_key`(`user_id`),
     INDEX `user_id`(`user_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
