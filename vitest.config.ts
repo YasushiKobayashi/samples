c0df0dc18e8694b1fed447b4b7af409dbc8b148e
YasushiKobayashi <ptpadan@gmail.com>
2025-03-09 22:01:44 +0900

    add codecov/vite-plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
@@ -7,7 +8,15 @@ export default defineConfig(async () => {
   // why esmのエラーが出るため遅延ロードを行う
   const tsconfigPaths = (await import('vite-tsconfig-paths')).default
   return {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      codecovVitePlugin({
+        enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+        bundleName: 'samples',
+        uploadToken: process.env.CODECOV_TOKEN,
+      }),
+    ],
     test: {
       globals: true,
       environment: 'happy-dom',
